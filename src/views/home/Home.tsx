2f37ed68a3f383b5848884f36661137f2bc87eaa
valeriapaulinalustres <valeriapaulinalustres@yahoo.com.ar>
2022-11-29 07:03:04 -0300

    borro texto de inicio

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -8,12 +8,7 @@ function Home() {
     <div className="home-container">
       <h1>JORNADAS DE SALUD EN MORÓN</h1>
       <h2>25 Y 26 DE OCTUBRE 2022</h2>
-      <p className="description-home">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-        doloribus consectetur dolor accusamus, veniam qui eius officiis ipsam
-        nesciunt optio perspiciatis totam recusandae! Sint perferendis ut
-        doloremque perspiciatis assumenda sed.
-      </p>
+   
       <div className="home-buttons-container">
         <Link
           to="./assets/programa.pdf"
@@ -51,3 +46,12 @@ function Home() {
 }
 
 export default Home;
+
+/*
+   <p className="description-home">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+        doloribus consectetur dolor accusamus, veniam qui eius officiis ipsam
+        nesciunt optio perspiciatis totam recusandae! Sint perferendis ut
+        doloremque perspiciatis assumenda sed.
+      </p>
+      */
